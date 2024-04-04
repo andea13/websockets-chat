da77a86ae8247e03b4e334a4c8681d0ff7c268aa
Oksana Dziuba <dziuba131303@gmail.com>
2024-04-04 16:36:06 +0400

    creates and styles Register component

--- Client/src/Register.jsx
+++ Client/src/Register.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    console.log(e.target.value);
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    console.log(e.target.value);
+    setPassword(e.target.value);
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="bg-blue-50 h-screen flex items-center">
+      <form className="w-64 mx-auto mb-12">
+        <input
+          onChange={handleUsernameChange}
+          value={username}
+          type="text"
+          placeholder="Username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <input
+          onChange={handlePasswordChange}
+          value={password}
+          type="password"
+          placeholder="Password"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
+          Register
+        </button>
+      </form>
     </div>
   );
 };
